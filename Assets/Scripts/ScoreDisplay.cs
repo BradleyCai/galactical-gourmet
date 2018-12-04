6d08412e879874608134f6064b1d705b8b27448e
Jonathan Ho <ho8584@gmail.com>
2018-12-03 18:54:04 -0800

    Score counting implementing, need the score to follow camera

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -7,17 +7,35 @@ public class ScoreDisplay : MonoBehaviour {
 	
 	public TextMesh scoreText;
 
+	private TextMesh score;
+	private bool update;
+	private int tempScore;
 	private int playerScore;
 
 	// Use this for initialization
 	void Start () {
-		playerScore = ConsumePlanet.playerScore;
+		update = false;
+		score = Instantiate(scoreText);
+		score.GetComponent<TextMesh>().text = "Score: " + ConsumePlanet.playerScore;
+		tempScore = ConsumePlanet.playerScore;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		TextMesh score = Instantiate(scoreText);
-		score.GetComponent<TextMesh>().text = "Score: " + playerScore;
+		Debug.Log(ConsumePlanet.playerScore);
+		if (tempScore < ConsumePlanet.playerScore) {
+			update = true;
+		}
+		if (update) {
+			updateScore();
+			tempScore = ConsumePlanet.playerScore;
+			update = false;
+		}
 	}
 	
+	void updateScore() {
+		Destroy(score);
+		score = Instantiate(scoreText);
+		score.GetComponent<TextMesh>().text = "Score: " + ConsumePlanet.playerScore;
+	}
 }
