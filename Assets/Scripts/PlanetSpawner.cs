333d082040112835d02826cbb82cce0ea1b6fca4
Jonathan Ho <ho8584@gmail.com>
2018-12-03 19:22:05 -0800

    Score updates now follows user and updates as the user collides with a planet

--- Assets/Scripts/PlanetSpawner.cs
+++ Assets/Scripts/PlanetSpawner.cs
@@ -9,13 +9,12 @@ public class PlanetSpawner : MonoBehaviour {
 	private float spawnDistance = 200f;
 	private float scaleLowerRange = 10f;
 	private float scaleUpperRange = 50f;
-	private static int planetCount;
 
     public int planetLimit; // set this value inside unity UI
 
 	// Use this for initialization
 	void Start () {
-		planetCount = ConsumePlanet.planetCount; // initiallize planetCount to 0
+		
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -7,35 +7,18 @@ public class ScoreDisplay : MonoBehaviour {
 	
 	public TextMesh scoreText;
 
-	private TextMesh score;
-	private bool update;
-	private int tempScore;
 	private int playerScore;
 
 	// Use this for initialization
-	void Start () {
-		update = false;
-		score = Instantiate(scoreText);
-		score.GetComponent<TextMesh>().text = "Score: " + ConsumePlanet.playerScore;
-		tempScore = ConsumePlanet.playerScore;
+	void Start () {	
+		scoreText.GetComponent<TextMesh>().text = "Score: 0";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Debug.Log(ConsumePlanet.playerScore);
-		if (tempScore < ConsumePlanet.playerScore) {
-			update = true;
-		}
-		if (update) {
-			updateScore();
-			tempScore = ConsumePlanet.playerScore;
-			update = false;
-		}
+		string input = "Score: " + ConsumePlanet.playerScore;
+		scoreText.GetComponent<TextMesh>().text = input;
 	}
 	
-	void updateScore() {
-		Destroy(score);
-		score = Instantiate(scoreText);
-		score.GetComponent<TextMesh>().text = "Score: " + ConsumePlanet.playerScore;
-	}
 }
