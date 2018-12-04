8804054aa87eaa81fefadaeebe1237a1673fa08d
Jonathan Ho <ho8584@gmail.com>
2018-12-03 17:34:05 -0800

    working on player score text

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+	
+	public TextMesh scoreText;
+
+	private int playerScore;
+
+	// Use this for initialization
+	void Start () {
+		playerScore = ConsumePlanet.playerScore;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TextMesh score = Instantiate(scoreText);
+		score.GetComponent<TextMesh>().text = "Score: " + playerScore;
+	}
+	
+}
