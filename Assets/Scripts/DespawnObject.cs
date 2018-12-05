4cfb58bc2307aea2e534d383409760e12538eb47
BradleyCai <bradcai24@gmail.com>
2018-12-05 02:38:26 -0800

    Rearrange the hierarchy and implement a DespawnField

---
+++ Assets/Scripts/DespawnObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnObject : MonoBehaviour {
+	void OnTriggerExit(Collider col) {
+		Destroy(col.gameObject);
+	}
+}
