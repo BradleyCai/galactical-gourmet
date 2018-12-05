c266a60ea1fa8d1d08654257df7586f06c12febd
BradleyCai <bradcai24@gmail.com>
2018-12-05 01:29:52 -0800

    Implement basic initial planet velocity in spawner

--- Assets/Scripts/PlanetSpawner.cs
+++ Assets/Scripts/PlanetSpawner.cs
@@ -7,6 +7,8 @@ public class PlanetSpawner : MonoBehaviour {
 	public Texture[] textures;
 	public GameObject planetPrefab;
     public int planetLimit;
+	public int planetSpeed;
+	public int planetSpeedVariance;
 
 	private float spawnDistance = 200f;
 	private float scaleLowerRange = 10f;
@@ -25,11 +27,16 @@ public class PlanetSpawner : MonoBehaviour {
 	void SpawnSpaceObj(GameObject objPrefab) {
 		Quaternion spawnDirection = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
 		float size = Random.Range(scaleLowerRange, scaleUpperRange);
+		Vector3 velocity;
 		
 		GameObject spaceObj = Instantiate(objPrefab);
 		spaceObj.transform.position = GetPosition(spawnDirection, transform.position, spawnDistance);
 		spaceObj.transform.rotation = new Quaternion(0, 0, 0, 1);
-		spaceObj.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+		velocity = -Vector3.Normalize(spaceObj.transform.position) * planetSpeed;
+		velocity.x += Random.Range(-planetSpeedVariance, planetSpeedVariance);
+		velocity.y += Random.Range(-planetSpeedVariance, planetSpeedVariance);
+		velocity.z += Random.Range(-planetSpeedVariance, planetSpeedVariance);
+		spaceObj.GetComponent<Rigidbody>().velocity = velocity;
 		spaceObj.transform.localScale = new Vector3(size, size, size);
 		spaceObj.GetComponent<Renderer>().material.mainTexture = textures[Random.Range(0, textures.Length)];
 	}
