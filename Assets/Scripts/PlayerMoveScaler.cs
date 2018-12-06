e1afcf60c2fcd923b22b986baa2a77b87d76dc74
BradleyCai <bradcai24@gmail.com>
2018-12-05 21:26:19 -0800

    Start work on player movement scale

---
+++ Assets/Scripts/PlayerMoveScaler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScaler : MonoBehaviour {
+    public GameObject player;
+    public GameData gameData;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // player.transform.position *= gameData.playerSize;
+        // player.transform.position = transform.position * 5;
+	}
+}
