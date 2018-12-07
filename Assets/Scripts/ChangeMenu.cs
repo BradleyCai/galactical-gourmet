96753c66963e5a6dc95c19121c6599af84b9219d
Jonathan Ho <ho8584@gmail.com>
2018-12-07 15:08:28 -0800

    Refactored code so menu selection is more smooth

--- Assets/Scripts/ChangeMenu.cs
+++ Assets/Scripts/ChangeMenu.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
 public class ChangeMenu : MonoBehaviour {
 
-
-	void start() {
-
+	public void PlayGame() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 
+	public void QuitGame() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+
+
+/**
 	void update() {
 		string buttonPressed = EventSystem.current.currentSelectedGameObject.name;
 
@@ -38,4 +49,5 @@ public class ChangeMenu : MonoBehaviour {
 		Application.Quit();
 		#endif
 	}
+**/
 }
