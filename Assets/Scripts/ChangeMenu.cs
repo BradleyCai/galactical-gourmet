86e8cab2a291821a52cd4e922ed6067a86ce4ba3
Jonathan Ho <ho8584@gmail.com>
2018-12-07 13:14:36 -0800

    Added basic start and exit button

---
+++ Assets/Scripts/ChangeMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChangeMenu : MonoBehaviour {
+
+
+	void start() {
+
+	}
+
+	void update() {
+		string buttonPressed = EventSystem.current.currentSelectedGameObject.name;
+
+		if (buttonPressed == "StartButton") {
+			changeMenu("PlayingScene");
+		}
+		else if (buttonPressed == "ExitButton") {
+			exitGame();
+		}
+	}
+
+	public void changeMenu(string sceneName) {
+		Application.LoadLevel(sceneName);
+	}
+
+	public void exitGame() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
